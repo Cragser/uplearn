4c5427939c018b347ee1ee3f73e50320a51e6c5e
Sergio Reyes <sergio.reyes@plexusworldwide.com>
2025-01-28 19:17:58 -0600

    2.6 (Temporal) Add ai services and examples

--- site-uplearn/src/app/page.tsx
+++ site-uplearn/src/app/page.tsx
@@ -1,10 +1,19 @@
 import { DeckSelector } from "@/src/client/components/deck-selector";
 import {getAnkiNextConnection} from "@/src/server/api/anki/anki-connection";
 import AnkiCardGallery from "@/src/client/module/anki/anki-card-gallery";
+import {getMoodleNextConnection} from "@/src/server/api/moodle/moodle-connection";
 
 async function getData() {
   const res = await fetch(getAnkiNextConnection('get-decks'));
   return res.json();
+}
+
+// todo:
+// https://github.com/WebHare/moodle-webservice#readme
+
+async function getMoodleCourses() {
+    const res = await fetch(getMoodleNextConnection('get-courses'));
+    return res.json();
 }
 
 export default async function HomePage() {
