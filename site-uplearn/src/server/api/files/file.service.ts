11fd7f1c091e7090240619f6a06fd6ddcc68f320
Sergio Reyes <sergio.reyes@plexusworldwide.com>
2025-02-03 04:34:48 -0600

    2.23 No save files in server

--- site-uplearn/src/server/api/files/file.service.ts
+++ site-uplearn/src/server/api/files/file.service.ts
@@ -6,10 +6,15 @@ export interface SaveResponseOptions {
   responseBody: unknown;
 }
 
+const IS_SERVER = process.env.IS_SERVER === "true";
 export async function saveResponseToFile({
   responseBody,
   userMessagePreview,
 }: SaveResponseOptions): Promise<void> {
+  if (IS_SERVER) {
+    return;
+  }
+
   const filesDir = path.join(process.cwd(), "files");
   if (!fs.existsSync(filesDir)) {
     fs.mkdirSync(filesDir, { recursive: true });
